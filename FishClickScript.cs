297c0871ea6de7331736d39569b939ace442fc32
Megan <94812744+Megan-Sullivan@users.noreply.github.com>
2023-03-05 14:49:17 -0800

    Add files via upload
    Fish Click Script is re-usable for future game objects, possibly every game object from here on out that needs to respond to being clicked/touched, there are sufficient comments to understand what each piece of the code does as well

--- FishClickScript.cs
+++ FishClickScript.cs
@@ -5,33 +5,33 @@ public class FishClickScript : MonoBehaviour
 {
     private Vector3 bigScale, smallScale;
 
-    public Text ScoreAmount;
-    public AudioSource FishSplash1;
-    private int scoreCount;
+    public Text ScoreAmount; //Assign ScoreAmount object to script
+    public AudioSource FishSplash1; //Assign Audio File to script
+    private int scoreCount; //Integer used with a default of 0 to add to score
 
-    private Vector3 mOffset;
+    private Vector3 mOffset; //Vector 3 offset used for mouse dragging script
 
-    private float mZCoord;
+    private float mZCoord; //Z coord variable for mouse dragging script
 
     void Start()
     {
-        bigScale = new Vector3(1.8f, 1.8f, 1.8f);
-        smallScale = new Vector3(1f, 1f, 1f);
+        bigScale = new Vector3(1.8f, 1.8f, 1.8f); //Make object big when called
+        smallScale = new Vector3(1f, 1f, 1f); //Make object small when called
 
-        scoreCount = 0;
-        ScoreAmount.text = "Score: " + scoreCount;
+        scoreCount = 0; //Default score of zero set as integer
+        ScoreAmount.text = "Score: " + scoreCount; //Assign Score string and Score Count in to Score Amount text object
     }
     
     private void OnMouseDown()
     {
-        scoreCount += 10;
-        ScoreAmount.text = "Score: " + scoreCount;
+        scoreCount += 10; //Adds 10 to score when mouse is clicked on object
+        ScoreAmount.text = "Score: " + scoreCount; //Updates the Score Amount text object with the new integer (+10) per click
 
-        FishSplash1.GetComponent<AudioSource>().Play();
+        FishSplash1.GetComponent<AudioSource>().Play(); //Play audio source when mouse is clicked
 
-        transform.localScale = bigScale;
+        transform.localScale = bigScale; //Call statement when mouse is clicked to make object big
 
-        mZCoord = Camera.main.WorldToScreenPoint(gameObject.transform.position).z;
+        mZCoord = Camera.main.WorldToScreenPoint(gameObject.transform.position).z; //Store game object transformation to mZCoord variable
 
         // Store offset = gameobject world pos - mouse world pos
         mOffset = gameObject.transform.position - GetMouseWorldPos();
@@ -40,10 +40,11 @@ public class FishClickScript : MonoBehaviour
 
     private void OnMouseUp()
     {
-        transform.localScale = smallScale;
+        transform.localScale = smallScale; // Transform object back to small size
 
     }
 
+    //This function is specifically for being able to drag the object while holding down the mouse button
     private Vector3 GetMouseWorldPos()
     {
         // pixel coordinates (x, y)
@@ -56,9 +57,10 @@ public class FishClickScript : MonoBehaviour
 
     }
 
+    //This function calls the above function during mouse dragging
     void OnMouseDrag()
     {
-        transform.position = GetMouseWorldPos() + mOffset;
+        transform.position = GetMouseWorldPos() + mOffset; //Changes position of game object using the GetMouseWorldPos() function and the mOffset variable
 
     }
 }
